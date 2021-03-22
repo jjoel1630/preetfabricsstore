38ffe742a08d7349aa3a82bc94f4c4f9eec31b92
Joel Johnson <jjoel1630@gmail.com>
2021-03-21 18:47:54 -0700

    added some things

---
+++ ourscripts/gallery.ts
@@ -0,0 +1,14 @@
+const gallerySwitcher = (evt, menuSection) => {
+  var galleryContent = document.querySelectorAll(".gallery-content");
+  for (var i = 0; i < galleryContent.length; i++) {
+    galleryContent[i].style.display = "none";
+  }
+  var galleryLinks = document.querySelectorAll(".gallery-links");
+  for (var i = 0; i < galleryLinks.length; i++) {
+    galleryLinks[i].className = galleryLinks[i].className.replace(" tab-links-active", "");
+  }
+  document.querySelector(`#${menuSection}`).style.display = "block";
+  evt.currentTarget.className += " tab-links-active";
+}
+
+document.querySelector("#defaultOpenGallery").click();
