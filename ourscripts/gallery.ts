74dc0a9f93dd52e02b4ff16cf63f9c46e9b71daf
Joel Johnson <jjoel1630@gmail.com>
2021-03-26 08:36:41 -0700

    fixed some things, but there are many bugs :bug:

--- ourscripts/gallery.ts
+++ ourscripts/gallery.ts
@@ -1,14 +1,23 @@
 const gallerySwitcher = (evt, menuSection) => {
+  //gallery-content = each page
   var galleryContent = document.querySelectorAll(".gallery-content");
+  //display none to each of the pages so we can display the correct one
   for (var i = 0; i < galleryContent.length; i++) {
     galleryContent[i].style.display = "none";
   }
+
+  //gallery-links = all the links (page 1, page 2, page 3)
   var galleryLinks = document.querySelectorAll(".gallery-links");
+  //replace the active class with nothing (if button is on page 1, it will be grey, but then it will replace the class of active to remove it)
   for (var i = 0; i < galleryLinks.length; i++) {
     galleryLinks[i].className = galleryLinks[i].className.replace(" tab-links-active", "");
   }
+
+  //get the display id which will be each different page, but we are only displaying the one we pass in
   document.querySelector(`#${menuSection}`).style.display = "block";
-  evt.currentTarget.className += " tab-links-active";
+  //give the targeted button an active class
+  evt.currentTarget.className += " item-links-active";
 }
 
+//when the page loads, automatically open the defualt one
 document.querySelector("#defaultOpenGallery").click();
--- ourscripts/items.js
+++ ourscripts/items.js
@@ -1,12 +1,12 @@
-var gallerySwitcher = function (evt, menuSection) {
+var itemSwitcher = function (evt, menuSection) {
     //gallery-content = each page
-    var galleryContent = document.querySelectorAll(".gallery-content");
+    var galleryContent = document.querySelectorAll(".item-content");
     //display none to each of the pages so we can display the correct one
     for (var i = 0; i < galleryContent.length; i++) {
         galleryContent[i].style.display = "none";
     }
     //gallery-links = all the links (page 1, page 2, page 3)
-    var galleryLinks = document.querySelectorAll(".gallery-links");
+    var galleryLinks = document.querySelectorAll(".item-links");
     //replace the active class with nothing (if button is on page 1, it will be grey, but then it will replace the class of active to remove it)
     for (var i = 0; i < galleryLinks.length; i++) {
         galleryLinks[i].className = galleryLinks[i].className.replace(" tab-links-active", "");
@@ -17,4 +17,4 @@ var gallerySwitcher = function (evt, menuSection) {
     evt.currentTarget.className += " tab-links-active";
 };
 //when the page loads, automatically open the defualt one
-document.querySelector("#defaultOpenGallery").click();
+document.querySelector("#defaultOpenItem").click();
