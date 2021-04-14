fa31317df542b552d01a534aeb01a9cc88a3e44b
Joel Johnson <jjoel1630@gmail.com>
2021-04-14 11:54:20 -0700

    major changes :boom:

--- ourscripts/nav.js
+++ ourscripts/nav.js
@@ -1,9 +1,28 @@
-window.onscroll = function () {
-    var navbar = document.querySelector("#navbar-sticky");
-    var sticky = navbar.offsetTop;
-    if (window.pageYOffset < sticky) {
+// window.onscroll = () => {
+//     const navbar = document.querySelector<HTMLElement>("#navbar-sticky");
+//     const sticky = navbar.offsetTop;
+//     if(window.pageYOffset < sticky) {
+//         navbar.classList.remove("sticky-nav")
+//         return;
+//     }
+//     navbar.classList.add("sticky-nav");
+// }
+document.addEventListener("scroll", function () {
+    var navbar = document.querySelector(".navbar-div");
+    var navbarHeight = 30;
+    var distanceFromTop = Math.abs(document.body.getBoundingClientRect().top);
+    if (distanceFromTop >= navbarHeight)
+        navbar.classList.add("sticky-nav");
+    else
         navbar.classList.remove("sticky-nav");
-        return;
+});
+var navbarToggle = document.querySelector("#navbar-responsive-toggle");
+var navbarDivResponsive = document.querySelector(".navbar-div-responsive");
+navbarToggle.addEventListener("click", function () {
+    if (navbarDivResponsive.style.display === "none" || navbarDivResponsive.style.display === "") {
+        navbarDivResponsive.style.display = "block";
     }
-    navbar.classList.add("sticky-nav");
-};
+    else if (navbarDivResponsive.style.display === "block") {
+        navbarDivResponsive.style.display = "none";
+    }
+});
--- ourscripts/nav.ts
+++ ourscripts/nav.ts
@@ -1,10 +1,32 @@
-window.onscroll = () => {
-    const navbar = document.querySelector<HTMLElement>("#navbar-sticky");
-    const sticky = navbar.offsetTop;
+// window.onscroll = () => {
+//     const navbar = document.querySelector<HTMLElement>("#navbar-sticky");
+//     const sticky = navbar.offsetTop;
 
-    if(window.pageYOffset < sticky) {
-        navbar.classList.remove("sticky-nav")
-        return;
+//     if(window.pageYOffset < sticky) {
+//         navbar.classList.remove("sticky-nav")
+//         return;
+//     }
+//     navbar.classList.add("sticky-nav");
+// }
+
+document.addEventListener("scroll", function () {
+    const navbar = document.querySelector<HTMLElement>(".navbar-div");
+    const navbarHeight = 30;
+  
+    const distanceFromTop = Math.abs(
+      document.body.getBoundingClientRect().top
+    );
+  
+    if (distanceFromTop >= navbarHeight) navbar.classList.add("sticky-nav");
+    else navbar.classList.remove("sticky-nav");
+});
+
+const navbarToggle = document.querySelector<HTMLElement>("#navbar-responsive-toggle");
+const navbarDivResponsive = document.querySelector<HTMLElement>(".navbar-div-responsive");
+navbarToggle.addEventListener("click", () => {
+    if(navbarDivResponsive.style.display === "none" || navbarDivResponsive.style.display === "") {
+        navbarDivResponsive.style.display = "block";
+    } else if(navbarDivResponsive.style.display === "block") {
+        navbarDivResponsive.style.display = "none";
     }
-    navbar.classList.add("sticky-nav");
-}
+})
